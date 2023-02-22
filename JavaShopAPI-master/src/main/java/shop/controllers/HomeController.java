74e8e182008c5d1b4bb5b942515161c22dff8f40
pavlo999 <80537100+pavlo999@users.noreply.github.com>
2023-02-22 18:00:50 +0200

    ading project

---
+++ JavaShopAPI-master/src/main/java/shop/controllers/HomeController.java
@@ -0,0 +1,10 @@
+package shop.controllers;
+
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@AllArgsConstructor
+public class HomeController {
+
+}
